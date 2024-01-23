764f2ae952cfb928d6c274835f1d7373d1cc830a
Amanuel Mandefrow <amanuelmandefro3@gmail.com>
2024-01-23 08:40:43 +0300

    some change to host

--- SmartScale/settings.py
+++ SmartScale/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-=y^v&&@x5oj9b-b=h6r44xw1!d6w%_-apw%tzrw4_-6g^x6*fu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
+
 
 
 # Application definition
